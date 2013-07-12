21418f7491dc0f944e30ad5f3c51ceb77c1e07cf
Olya Flores <olya.flores@gmail.com>
2013-07-12 04:01:26 +0000

    Modified web.js to pull from file

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var databuf = new Buffer(50);
-    fs.readFileSync('index.html', function(err,databuf){
+    fs.readFileSync('index.html', 'utf8', function(err,databuf){
       if(err)throw err;
       console.log(databuf);
   });
