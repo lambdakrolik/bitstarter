15de48468b5f6b4cdd13a2307810b02598b74ae6
Olya Flores <olya.flores@gmail.com>
2013-07-12 04:26:31 +0000

    Modified web.js to pull from file

--- web.js
+++ web.js
@@ -4,12 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var databuf = new Buffer(50);
-var datastring;    
-fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf8', function(err,databuf){
-      if(err)throw err;
-      console.log(databuf);
-  });
+    var databuf = fs.readFileSync('index.html');
+
 
 response.send(databuf.toString('utf-8'));
 });
