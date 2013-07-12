fa6078b1c85d01640643fbefcb24060e5138a7cd
Olya Flores <olya.flores@gmail.com>
2013-07-12 03:40:54 +0000

    Modified web.js to pull from file

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  var fs = require('fs');
+    var data = new Buffer(50);
+    fs.readFileSync('index.html', function(err,data){
+      if(err)throw err;
+      console.log(data);
+  });
+response.send(data.toString('utf-8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
